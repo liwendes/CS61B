b42100ee6c3279ad465a3e1f39c20fd7ee0739bc
Wende Li <liwendes@gmail.com>
2021-01-31 03:20:43 -0500

    done with Collatz

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -2,9 +2,17 @@
  *  @author YOUR NAME HERE
  */
 public class Collatz {
+    /** Given a number, return the Collatz sequence */
+    public static int nextNumber(int n) {
+        if (n%2 == 0) {
+            return n/2;
+        } else {
+            return 3*n+1;
+        }
+    }
     public static void main(String[] args) {
         int n = 5;
-        System.out.print(n + " ");
+        System.out.print(nextNumber(n));
 
     }
 }
